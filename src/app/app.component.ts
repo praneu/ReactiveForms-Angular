a7ec7fdeb979215e029ab94318af2d5549c0516e
praneu <prazolbikneup@gmail.com>
2023-07-01 18:47:31 -0400

    'ReactiveForms'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Reactive-forms';
+ registrationForm = new FormGroup({
+  userName: new FormControl(''),
+  password: new FormControl(''),
+  confirmPassword: new FormControl('')
+});
+
+
 }
